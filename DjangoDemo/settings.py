daf5716a78303d2d11fc9a47b141084e84e8d376
mayuejiang <yuejiang_ma@163.com>
2021-08-31 00:09:09 +0800

    Web

--- DjangoDemo/settings.py
+++ DjangoDemo/settings.py
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DjangoDemo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -80,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
